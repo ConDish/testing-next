12ac62925d099099759f0c5cd7e2741465164513
ConDish <samioff15@gmail.com>
2019-03-08 09:04:22 -0500

    First commit deploy

--- pages/about.js
+++ pages/about.js
@@ -18,7 +18,7 @@ class AboutPage extends Component {
             <strong>{this.props.isServer ? "server" : "client"} side</strong>.
           </p>
           <p>
-            You can reload to see how the page change.
+            Hello World
           </p>
           <Link href="/">
             <a>Go to Home</a>
