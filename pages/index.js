66d7f3b196cdcf2095dab3d820e494c757d778f3
ConDish <samioff15@gmail.com>
2019-04-08 11:56:48 -0500

    Change in material ui

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import Link from "next/link";
 import Header from "../components/header";
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/core/Menu';
+import { withStyles } from '@material-ui/core/styles';
 
 function Index() {
   return (
     <main>
       <Header />
       <section>
-        <Link href="/about">
-          <a>Go to About Me</a>
-        </Link>
+
       </section>
     </main>
   );
